6443d7c6fe33f6b56995b0cb23c1d76bd0657339
Vsevolod Lutovinov <klopp@yandex.ru>
2023-04-16 03:01:40 +0300

    log issues

--- dir-mon.pl
+++ dir-mon.pl
@@ -3,7 +3,8 @@
 # ------------------------------------------------------------------------------
 use utf8::all;
 use open qw/:std :utf8/;
-use Modern::Perl;
+use strict;
+use warnings;
 
 # ------------------------------------------------------------------------------
 use Const::Fast;
@@ -13,7 +14,7 @@ use File::Which;
 use Getopt::Long qw/GetOptions/;
 use IPC::Open2;
 use IPC::Run qw/run/;
-use POSIX qw/:sys_wait_h/;
+use POSIX qw/:sys_wait_h strftime/;
 use Proc::Killfam;
 use Text::ParseWords qw/quotewords/;
 use Time::Local qw/timelocal_posix/;
@@ -29,7 +30,7 @@ my ( $TIMEOUT, $PATH, $EXEC, $QUIET, $DEBUG, $EXIT, $DRY ) = ( 60 * 10 );
 
 my $inotifywait = which $INOTYFY;
 if ( !$inotifywait ) {
-    say "\nNo required '$INOTYFY' executable found!";
+    print "No required '$INOTYFY' executable found!\n";
     exit 1;
 }
 
@@ -79,7 +80,7 @@ while (<$stdout>) {
     }xsm;
 
     my $taccess = timelocal_posix( $6, $5, $4, $3, $2 - 1, $1 - 1900 );
-    $DEBUG and printf "[%04d-%02d-%02d %02d:%02d:%02d] %s {%s}\n", $1, $2, $3, $4, $5, $6, $7, $8;
+    _d( '[%04d-%02d-%02d %02d:%02d:%02d] %s {%s}', $1, $2, $3, $4, $5, $6, $7, $8 );
     _check_access_time( undef, $taccess );
 }
 
@@ -90,14 +91,14 @@ sub _check_access_time
 
     my $tdiff = $taccess ? $taccess - $last_access : time - $last_access;
     if ( $tdiff > 0 ) {
-        $QUIET or printf "No activity: %u sec\n", $tdiff;
+        _i('No activity for %u sec', $tdiff );
         if ( $tdiff >= $TIMEOUT ) {
-            $QUIET or print "Timeout!\n";
-            ( $DEBUG or $DRY ) and printf "{run}\n> %s\n", ( join "\n> ", @EXECUTABLE );
+            _i('Timeout!');
+            ( $DEBUG or $DRY ) and _log('{run}\n> %s', ( join "\n> ", @EXECUTABLE ) );
             my ( $out, $err );
             $DRY or run \@EXECUTABLE, sub { }, \$out, \$err;
-            $out  and print $out;
-            $err  and print $err;
+            $out  and _log('%s', _trim($out));
+            $err  and _log('%s', _trim($err));
             $EXIT and _term();
             $last_access = time;
         }
@@ -165,6 +166,37 @@ sub _term
 # ------------------------------------------------------------------------------
 END {
     _term();
+}
+
+# ------------------------------------------------------------------------------
+sub _trim
+{
+    $_[0] =~ s/^\s+|\s+$//gsm;
+}
+
+# ------------------------------------------------------------------------------
+sub _t
+{
+    return strftime '%F %X ', localtime;
+}
+
+# ------------------------------------------------------------------------------
+sub _log
+{
+    my ( $fmt, @arg ) = @_;
+    printf "%s %s\n", _t(), $fmt, @arg;
+}
+
+# ------------------------------------------------------------------------------
+sub _i
+{
+    $QUIET or goto &_log;
+}
+
+# ------------------------------------------------------------------------------
+sub _d
+{
+    $DEBUG and goto &_log;
 }
 
 # ------------------------------------------------------------------------------
