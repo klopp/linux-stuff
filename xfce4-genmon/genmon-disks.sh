6c990bbb10b88231e83f2e6cdaf5a050aa4bfb1e
Vsevolod Lutovinov <klopp@yandex.ru>
2023-04-23 09:04:22 +0300

    --disk => --dev (optional)

--- xfce4-genmon/genmon-disks.sh
+++ xfce4-genmon/genmon-disks.sh
@@ -19,10 +19,10 @@ function usage
 {
     cat << USAGE
 Usage: $(basename "${0}") [options], where options are:
-    -d, --disk
-        Disk letter (REQUIRED, "a" => "/dev/sda", "b" => "/dev/sdb", etc)
-    -p, --part
-        Disk partition (default is "/home")
+    -d, --dev
+        Block device ID ("/dev/sda", "/dev/sdb", etc)
+    -p, --path
+        Path of partition (default is "/home")
     -t, --tmax
         "Red" temperature (Celsius, default is 50)
     -c, --click
@@ -56,12 +56,12 @@ function get_img
 # ------------------------------------------------------------------------------
 while [ $# -gt 0 ]; do
     case "${1}" in
-        '-d' | '--disk')
+        '-d' | '--dev')
             DEV="${2}"
             shift 2
             continue
         ;;
-        '-p' | '--part')
+        '-p' | '--path')
             PART="${2}"
             shift 2
             continue
@@ -87,11 +87,16 @@ while [ $# -gt 0 ]; do
 done
 
 if [ -z "${DEV}" ]; then
-    usage
+    DEV=$(realpath -q $(df ${PART} 2>/dev/null | awk 'NR>1 {print $1}') 2>/dev/null)
+    if [ -z "${DEV}" ]; then
+        DEV="?"
+    else
+        DEV=$(lsblk ${DEV} | awk 'NR>1 {print $1}' | cut -d' ' -f1)
+    fi
 fi
 
 # ------------------------------------------------------------------------------
-TEMPERATURE=$( check_int $(sudo smartctl -A /dev/sd${DEV} | grep -i temperature | awk '{print $10}') )
+TEMPERATURE=$( check_int $(sudo smartctl -A /dev/${DEV} | grep -i temperature | awk '{print $10}') )
 if ((${TEMPERATURE} == 0 )); then
     TEMPERATURE="?"
     GREEN="red"
@@ -116,7 +121,7 @@ else
     GREEN="red"
 fi
 
-TOOLTIP="┌ <span weight='bold' fgcolor='blue'>${PART}</span> on <span fgcolor='blue'>/dev/sd${DEV}</span>\n"
+TOOLTIP="┌ <span weight='bold' fgcolor='blue'>${PART}</span> on <span fgcolor='blue'>/dev/${DEV}</span>\n"
 TOOLTIP+="├─ Total\t\t\t: ${TOTAL}\n"
 TOOLTIP+="├─ Used\t\t\t: ${USED}\n"
 TOOLTIP+="├─ Free\t\t\t\t: ${FREE}\n"
