8e01c53f2f9c5f5c48fd5beb2eb0642b6a71c0cb
Vsevolod Lutovinov <klopp@yandex.ru>
2023-06-27 08:47:00 +0300

    use Things::Instance::LockSock

--- dir-mon.pl
+++ dir-mon.pl
@@ -11,43 +11,32 @@ use warnings;
 use Const::Fast;
 use Digest::MD5 qw/md5_hex/;
 use English qw/-no_match_vars/;
-use Fcntl qw/:DEFAULT :flock/;
 use File::Basename qw/basename/;
-use File::Which;
 use Getopt::Long qw/GetOptions/;
-use IPC::Open2;
 use IPC::Run qw/start/;
-use Lock::Socket qw/try_lock_socket/;
-use Net::EmptyPort qw/empty_port/;
 use POSIX qw/:sys_wait_h strftime setsid/;
 use Proc::Killfam;
 use Text::ParseWords qw/quotewords/;
-use Time::Local qw/timelocal_posix/;
-use Time::Out qw/timeout/;
+
+use Things::I2S;
+use Things::Inotify;
+use Things::Instance::LockSock;
+use Things::Trim;
 
 # ------------------------------------------------------------------------------
-our $VERSION = 'v1.05';
+our $VERSION = 'v2.00';
 const my $DEF_TIMEOUT  => ( 60 * 10 );
 const my $DEF_INTERVAL => 10;
 const my $DEF_LOCKDIR  => '/var/lock';
 const my $EXE_NAME     => basename($PROGRAM_NAME);
-const my $INOTYFY      => 'inotifywait';
-const my $RX_DATE      => '(\d{4})[-](\d\d)[-](\d\d)';
-const my $RX_TIME      => '(\d\d):(\d\d):(\d\d)';
 
 # ------------------------------------------------------------------------------
-my ( $cpid, $last_access, $ipid );
+my ( $cpid, $last_access );
 my %opt = (
     timeout  => $DEF_TIMEOUT,
     interval => $DEF_INTERVAL,
     lockdir  => $DEF_LOCKDIR,
 );
-
-my $inotifywait = which $INOTYFY;
-if ( !$inotifywait ) {
-    print "No required '$INOTYFY' executable found!\n";
-    exit 1;
-}
 
 # ------------------------------------------------------------------------------
 GetOptions(
@@ -66,20 +55,21 @@ for (@EXECUTABLE) {
 my $LOCKFILE = sprintf '%s/%s.%s', $opt{lockdir}, $EXE_NAME, md5_hex( $opt{path}, $opt{exec} );
 local $OUTPUT_AUTOFLUSH = 1;
 
-my $lock = _lock_instance($LOCKFILE);
+my $watcher = Things::Inotify->new(
+    recurse => 1,
+    path    => $opt{path},
+    mode    => 'f',
+);
+if ( $watcher->error ) {
+    _log( q{!}, '%s', $watcher->error );
+    $cpid = $PID;
+    exit 1;
+}
+
+my $lock = Things::Instance::LockSock->new->lock( file => $LOCKFILE );
+
 if ( $lock->{errno} ) {
-    if ( $lock->{reason} eq 'open' ) {
-        _log( q{!}, 'Can not open file "%s" (%s)!', $LOCKFILE, $lock->{errno} );
-    }
-    elsif ( $lock->{reason} eq 'lock' ) {
-        _log( q{!}, 'Can not lock process on port %u (%s)!', $lock->{port}, $lock->{errno} );
-    }
-    elsif ( $lock->{reason} eq 'write' ) {
-        _log( q{!}, 'Can not write file "%s" (%s)!', $LOCKFILE, $lock->{errno} );
-    }
-    else {
-        _log( q{!}, 'Unknown error reason (%s)!', $lock->{errno} );
-    }
+    _log( q{!}, '%s', $lock->{msg} );
     $cpid = $PID;
     exit 1;
 }
@@ -103,32 +93,13 @@ else {
 use sigtrap qw/handler _signal_x normal-signals error-signals USR1 USR2/;
 use sigtrap qw/handler _check_access_time ALRM/;
 
-my $icmd = sprintf '%s -q -m -r --timefmt="%%Y-%%m-%%d %%X" --format="%%T %%w%%f [%%e]" "%s"', $inotifywait, $opt{path};
-_i( 'Run %s...', $icmd );
-$ipid        = open2( my $stdout, undef, $icmd );
 $last_access = time;
 alarm $opt{interval};
-while (1) {
-
-    $_ = timeout $opt{interval} => sub { $stdout->getline };
-    while ($_) {
-        my $taccess;
-        if (m{^
-            $RX_DATE
-            \s+
-            $RX_TIME
-            \s+
-            (.*)
-            \s+
-            \[(.+)\]
-        }xsm
-            )
-        {
-            $taccess = timelocal_posix( $6, $5, $4, $3, $2 - 1, $1 - 1900 );
-            _d( '{%s} {%s} {%s}', _t($taccess), $7, $8 );
-        }
-        _check_access_time( undef, $taccess );
-        $_ = timeout $opt{interval} => sub { $stdout->getline };
+$watcher->run();
+while ( my @events = $watcher->wait_for_events() ) {
+    for (@events) {
+        _d( '{%s} {%s} [%s]', _t( $_->{tstamp} ), $_->{path}, join( q{,}, @{ $_->{events} } ) );
+        _check_access_time( undef, $_->{tstamp} );
     }
 }
 
@@ -161,8 +132,8 @@ sub _check_access_time
                 $h->finish;
                 $rc = $h->full_result;
             }
-            $out and _i( '%s', _trim($out) );
-            $err and _i( '%s', _trim($err) );
+            $out and _i( '%s', trim($out) );
+            $err and _i( '%s', trim($err) );
             if ( $opt{x} || $opt{xx} ) {
                 if ( !$opt{xx} ) {
                     if ($rc) {
@@ -197,55 +168,6 @@ sub _opt_error
 }
 
 # ------------------------------------------------------------------------------
-# TODO :: move this function to private PERL5LIB
-#
-# PART:
-#   \d+[s] - seconds, \d+[m] - minutes, \d+[h] - hours, \d+[d] - days
-# IN string:
-#   PART[{, }PART...]
-# Example:
-#   "1d, 24m, 3h, 30s"
-#
-# OR
-#   "23:3:6:15" => 23 days, 3 hours, 6 minutes, 15 seconds
-#   "3:6:15"    => 3 hours, 6 minutes, 15 seconds
-#   etc
-# ------------------------------------------------------------------------------
-sub _interval_to_seconds
-{
-    my ($interval) = @_;
-    my $seconds = 0;
-    my @parts;
-
-    if ( $interval =~ /^(\d+[:]?)+$/gsm ) {
-        @parts = split /[:]/, $interval;
-        $seconds += pop @parts;
-        $seconds += pop(@parts) * 60           if @parts;
-        $seconds += pop(@parts) * 60 * 60      if @parts;
-        $seconds += pop(@parts) * 60 * 60 * 24 if @parts;
-        return $seconds;
-    }
-
-    @parts = split /[,\s]+/sm, lc $interval;
-    for (@parts) {
-        return unless /^(\d+)([smhd]?)$/sm;
-        if ( $2 eq 'm' ) {
-            $seconds += $1 * 60;
-        }
-        elsif ( $2 eq 'h' ) {
-            $seconds += $1 * 60 * 60;
-        }
-        elsif ( $2 eq 'd' ) {
-            $seconds += $1 * 60 * 60 * 24;
-        }
-        else {
-            $seconds += $1;
-        }
-    }
-    return $seconds;
-}
-
-# ------------------------------------------------------------------------------
 sub _check_opt
 {
     if ( !$opt{path} || !-d $opt{path} ) {
@@ -257,8 +179,8 @@ sub _check_opt
     if ( $opt{lockdir} && !-d $opt{lockdir} ) {
         return _opt_error('lock');
     }
-    $opt{interval} = _interval_to_seconds( $opt{interval} );
-    $opt{timeout}  = _interval_to_seconds( $opt{timeout} );
+    $opt{interval} = interval_to_seconds( $opt{interval} );
+    $opt{timeout}  = interval_to_seconds( $opt{timeout} );
     $opt{interval} or return _opt_error('interval');
     $opt{timeout}  or return _opt_error('timeout');
     ( $opt{interval} < 10 || $opt{interval} > 60 ) and return _opt_error('interval');
@@ -320,51 +242,9 @@ sub _signal_x
 }
 
 # ------------------------------------------------------------------------------
-sub _lock_instance
-{
-    my ($lockfile) = @_;
-
-    my ( $lock, $port, $fh );
-    if ( !sysopen $fh, $lockfile, O_RDWR | O_CREAT ) {
-        return { reason => 'open', errno => $ERRNO, };
-    }
-    binmode $fh;
-    sysread $fh, $port, 64;
-    $port and $port =~ s/^\s+|\s+$//gsm;
-    if ( !$port || $port !~ /^\d+$/sm ) {
-        $port = empty_port();
-    }
-    if ( !( $lock = try_lock_socket($port) ) ) {
-        close $fh;
-        return { port => $port, reason => 'lock', errno => $ERRNO, };
-    }
-    if ( !flock( $fh, LOCK_EX ) || !truncate( $fh, 0 ) || !syswrite( $fh, $port, length $port ) ) {
-        close $fh;
-        return { reason => 'write', errno => $ERRNO, };
-    }
-    close $fh;
-    return $lock;
-}
-
-# ------------------------------------------------------------------------------
 END {
     $cpid or _log( q{!}, 'Stop all jobs and exit...' );
-    if ($ipid) {
-        _d( 'Send TERM to [%u] pid tree...', $ipid );
-        killfam 'TERM', ($ipid);
-        while ( ( my $kidpid = waitpid -1, WNOHANG ) > 0 ) {
-            sleep 1;
-        }
-        unlink $LOCKFILE;
-    }
-}
-
-# ------------------------------------------------------------------------------
-sub _trim
-{
-    my ($s) = @_;
-    $s and $s =~ s/^\s+|\s+$//gsm;
-    return $s;
+    $LOCKFILE and unlink $LOCKFILE;
 }
 
 # ------------------------------------------------------------------------------
